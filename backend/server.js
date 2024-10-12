d1dce0159c1adaf3b5ceecf48a3ccdb8787051da
Ayushswirlon <ayushcarpenter7777@gmail.com>
2024-10-12 13:25:32 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://clg-event-management.vercel.app/'],
+  origin: ['clg-event-management-uofw-ptw0ru66h-ayush-carpetners-projects.vercel.app', 'https://clg-event-management.vercel.app/'],
   optionsSuccessStatus: 200
 };
 
